26f46fd3059ce017912315ad1185ba946eb1408e
Kyle Mosier <mosierkyle@gmail.com>
2023-08-20 20:24:49 -0700

    Style header, main content, and individual book
    Styled the header grid and styled the main content grid. Also made the template for each book.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const myLibray = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => `${this.title} by ${this.author} with ${this.pages} pages. ${this.read ? 'Read.' : "Havn't read."} `
+}
+
+const unscripted = new book('Unscripted', 'Mj Demarco', 350, true);
+const neverFinished = new book('Never Finished', 'David Goggins', 300, true);
+
