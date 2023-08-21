a171c4618245bb552ba6949423c8529bc18e8f8e
Kyle Mosier <mosierkyle@gmail.com>
2023-08-20 20:35:43 -0700

    Create add book to library function
    Created a function that takes in user inputs and adds them to the library as an object.

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 
 const myLibray = [];
 
-function book(title, author, pages, read) {
+function book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = () => `${this.title} by ${this.author} with ${this.pages} pages. ${this.read ? 'Read.' : "Havn't read."} `
+    this.status = status;
+    this.info = () => `${this.title} by ${this.author} with ${this.pages} pages. ${this.status}`
 }
+
+function addBookToLibrary(title,author,pages,status) {
+    let newBook = new book(title, author, pages, status)
+    myLibray.push(newBook);
+    console.log(myLibray);
+}
+
+addBookToLibrary('The Almanack of Naval Ravikant', 'Eric Jorgenson', 242, 'read')
 
 const unscripted = new book('Unscripted', 'Mj Demarco', 350, true);
 const neverFinished = new book('Never Finished', 'David Goggins', 300, true);
